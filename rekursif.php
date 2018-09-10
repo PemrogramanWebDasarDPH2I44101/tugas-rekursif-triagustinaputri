f268cc849d991f45dce6deffc2d409cca5feacbd
triagustinaputri <42725273+triagustinaputri@users.noreply.github.com>
2018-09-10 10:14:21 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,32 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function rekursif($kolom){
+
+  global $i;
+
+    rekursif2($i, $kolom);
+
+    echo "<br>";
+
+    if (--$kolom > $i){
+
+        rekursif($kolom);
+
+    }
+
 }
-rekursif(5);
+
+function rekursif2($j, $kolom) {
+
+	global $i;
+
+    if ($j > $i - $kolom) {
+
+        echo "*";
+
+        rekursif2($j - 1, $kolom);
+
+    }
+
+}
 ?>
